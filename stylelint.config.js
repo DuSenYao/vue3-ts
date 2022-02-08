0a10f8ea836b358e339c587900ed47220660ced4
dusenyao <1758231591@qq.com>
2022-02-08 15:58:41 +0800

    1. 增加 eslint、prettier、stylelint 校验 2. 安装 @types/node 声明包 3. vite 不需要 sass-loader 插件 删除

---
+++ stylelint.config.js
@@ -0,0 +1,86 @@
+module.exports = {
+  root: true,
+  defaultSeverity: 'warning',
+  extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-sass-guidelines',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier'
+  ],
+  rules: {
+    'selector-class-pattern': null,
+    'order/properties-alphabetical-order': null,
+    'selector-max-id': 1,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global']
+      }
+    ],
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep']
+      }
+    ],
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+          'function',
+          'if',
+          'each',
+          'include',
+          'mixin'
+        ]
+      }
+    ],
+    'no-empty-source': null,
+    'named-grid-areas-no-invalid': null,
+    'unicode-bom': 'never',
+    'no-descending-specificity': null,
+    'font-family-no-missing-generic-family-keyword': null,
+    'declaration-colon-space-after': 'always-single-line',
+    'declaration-colon-space-before': 'never',
+    'declaration-block-trailing-semicolon': 'always',
+    'rule-empty-line-before': [
+      'always',
+      {
+        ignore: ['after-comment', 'first-nested']
+      }
+    ],
+    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }]
+  },
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
+  overrides: [
+    {
+      files: ['*.vue', '**/*.vue'],
+      extends: [
+        'stylelint-config-recommended-scss',
+        'stylelint-config-sass-guidelines',
+        'stylelint-config-recommended-vue',
+        'stylelint-config-recess-order'
+      ],
+      rules: {
+        'keyframes-name-pattern': null,
+        'selector-pseudo-class-no-unknown': [
+          true,
+          {
+            ignorePseudoClasses: ['deep', 'global']
+          }
+        ],
+        'selector-pseudo-element-no-unknown': [
+          true,
+          {
+            ignorePseudoElements: ['v-deep', 'v-global', 'v-slotted']
+          }
+        ]
+      }
+    }
+  ]
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: pathResolve('src') + '/'
+      }
+    ]
+  },
+  server: {
+    port: 3333,
+    open: true
+  },
+  build: {
+    target: 'es2015',
+    cssTarget: 'chrome80',
+    outDir: 'dist',
+    // minify: 'terser',
+    /**
+     * 当 minify=“minify:'terser'” 解开注释
+     * Uncomment when minify="minify:'terser'"
+     */
+    // terserOptions: {
+    //   compress: {
+    //     keep_infinity: true,
+    //     drop_console: VITE_DROP_CONSOLE,
+    //   },
+    // },
+    // Turning off brotliSize display can slightly reduce packaging time
+    brotliSize: false,
+    chunkSizeWarningLimit: 2000
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 为 scss 配置共享全局变量
+        additionalData: `@import "@/styles/variable.scss";`
+      }
+    }
+  }
+});
