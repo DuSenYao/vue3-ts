65f2af1e84b04e44d1e2fc1239397a1069387d5f
dusenyao <1758231591@qq.com>
2022-02-08 12:57:11 +0800

    项目初始化

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  printWidth: 120,
+  vueIndentScriptAndStyle: false,
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+};
