b60ce58e77380c0cffaf33f1aa478272e8c070de
dusenyao <1758231591@qq.com>
2022-06-05 01:30:18 +0800

    升级依赖

--- src/components/TodoList/useTodos.ts
+++ src/components/TodoList/useTodos.ts
@@ -2,16 +2,16 @@
 import { ref, computed, Ref } from 'vue';
 import { useStorage } from '/@/utils/storage';
 
+interface Todo {
+  title: string;
+  done: boolean;
+}
+
 // 把和清单相关的所有数据和方法，都放在函数内部定义并且返回，这样这个函数就可以放在任意的地方来维护。
 export function useTodos() {
   // ref 函数使任何响应式变量在任何地方起作用，ref 接收参数并将其包裹在一个带有 value property 的对象中返回，然后可以使用该 property 访问或更改响应式变量的值
   const title = ref(''); // { value: '' }
   const showModal = ref(false);
-
-  interface Todo {
-    title: string;
-    done: boolean;
-  }
 
   const todos: Ref<Todo[]> = ref(useStorage('todos', []));
 
