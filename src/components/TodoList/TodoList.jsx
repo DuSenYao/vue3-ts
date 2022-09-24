d400c673670832043a1493f5539acdce58c28d7c
dusenyao <1758231591@qq.com>
2022-09-24 18:52:17 +0800

    1. 更新依赖 2. 优化 xlsx.vue

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -14,14 +14,15 @@ export default defineComponent({
       });
       title.value = '';
     }
-    // 在 setup 返回的 JSX 中，使用 vModel 取代 v-model，并且使用单个大括号包裹的形式传入变量 title.value ，然后使用 onClick 取代 @click。循环渲染清单的时候，使用 .map 映射取代 v-for，使用三元表达式取代 v-if。
+    // 在 setup 返回的 JSX 中，使用 vModel 取代 v-model，并且使用单个大括号包裹的形式传入变量 title.value
+    // 然后使用 onClick 取代 @click。循环渲染清单的时候，使用 .map 映射取代 v-for，使用三元表达式取代 v-if。
     return () => (
       <div>
-        <input type="text" v-model={title.value} />
+        <input type='text' v-model={title.value} />
         <button onClick={addTodo}>click</button>
         <ul>
           {todos.value.length ? (
-            todos.value.map(todo => {
+            todos.value.map((todo) => {
               return <li>{todo.title}</li>;
             })
           ) : (
