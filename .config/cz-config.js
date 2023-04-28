6e722d28c0539174f720fee829fa2e9305b2d1a8
dusenyao <1758231591@qq.com>
2023-04-28 14:49:53 +0800

    chore(custom): cz-config.js 修改

--- .config/cz-config.js
+++ .config/cz-config.js
@@ -14,14 +14,14 @@ module.exports = {
     { value: 'chore', name: 'chore:    构建过程或辅助工具的变动' },
     { value: 'revert', name: 'revert:   回退' }
   ],
+  scopes: [{ name: 'custom' }, { name: 'api' }, { name: 'upgrade' }],
   // 消息步骤
   messages: {
     type: '请选择提交类型：',
-    customScope: '请输入修改范围（可选）：',
+    customScope: '请输入修改范围（可选）:',
     subject: '请简要描述提交（必填）：',
     confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
   },
-  // 跳过问题
   skipQuestions: ['body', 'footer'],
   // subject 文字长度默认是 72
   subjectLimit: 100
